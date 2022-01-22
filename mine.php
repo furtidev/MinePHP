b70d34aec9088bec4fb0f8370a317a7cbd3e2b62
isferhossain <44488750+isferhossain@users.noreply.github.com>
2022-01-22 20:19:27 +0600

    updated to v1.2
    Added map, plugins and mods command.

--- mine.php
+++ mine.php
@@ -1,9 +1,9 @@
 <?php
 /*
 	Isfer Hossain
-	11th of November, 2021 
+	22th of January, 2022
 	- Please read the documentation before continuing
-	(version 1.1)
+	(version 1.2)
 */
 class MinePHP {
 	private $call_api;
@@ -17,6 +17,9 @@ class MinePHP {
 	public $ip;
 	public $port;
 	public $motd;
+	public $map;
+	public $plugins;
+	public $mods;
 	function __construct($server_ip = null, $software = null) {
 		$this->software_name = $software;
 		if ($software == "bedrock") {
@@ -46,10 +49,30 @@ class MinePHP {
 			$this->protocol = $this->call_api->protocol;
 			$this->version = $this->call_api->version;
 			if ($this->software_name == "java"){
-				$this->icon = $this->call_api->icon;
+				if (isset($this->call_api->icon)){
+					$this->icon = $this->call_api->icon;
+				} else {
+					$this->icon = "icon data not available";
+				}
 			}
 			$this->ip = $this->call_api->ip;
 			$this->port = $this->call_api->port;
+			$this->motd = $this->call_api->motd->html;
+			if (isset($this->call_api->icon)){
+				$this->map = $this->call_api->map;
+			} else {
+				$this->map = "map data not available";
+			}
+			if (isset($this->call_api->plugins->names)){
+				$this->plugins = $this->call_api->plugins->names;
+			} else {
+				$this->plugins = "plugin data not available";
+			}
+			if (isset($this->call_api->mods->names)){
+				$this->mods = $this->call_api->mods->names;
+			} else {
+				$this->mods = "mod data not available";
+			}
 		}
 	}
 }
